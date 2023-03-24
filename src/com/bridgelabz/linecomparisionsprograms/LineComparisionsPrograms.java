0c9634e76ee95e34f7665942a6383a992cdae0d8
sahana199835 <tnsahana1@gmail.com>
2023-03-24 13:08:07 +0530

    add:.gitignore and README.md files are added

---
+++ src/com/bridgelabz/linecomparisionsprograms/LineComparisionsPrograms.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linecomparisionsprograms;
+
+public class LineComparisionsPrograms {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome message for line comparision program");
+
+	}
+
+}
